99240e8bef196f15dd0394afce0bee48bef68b5c
remax21 <deronzier.remi@gmail.com>
2023-04-11 00:48:36 +0200

    :art: (Navigation) Make easier background color change for button menu
    Change Link by NavLink in react router dom

--- src/components/navbar/NavBarContent.jsx
+++ src/components/navbar/NavBarContent.jsx
@@ -1,40 +1,32 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 import ProfileDropdown from "./ProfileDropdown";
 import MobileMenuButton from "./mobile/MobileMenuButton";
 
-export default function NavBarContent({
-  navigation,
-  classNames,
-  user,
-  open,
-  updateNavigation,
-}) {
+export default function NavBarContent({ navigation, user, open }) {
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="flex h-16 items-center justify-between">
         <div className="flex items-center">
           <div className="flex-shrink-0">
-            <Link to="/" onClick={() => updateNavigation("/")}>
+            <NavLink to="/">
               <img className="h-8 w-8" src={logo} alt="Mediscreen" />
-            </Link>
+            </NavLink>
           </div>
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4">
               {navigation.map((item) => (
-                <Link
+                <NavLink
                   to={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "px-3 py-2 rounded-md text-sm font-medium"
-                  )}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  }
                   key={item.name}
-                  onClick={() => updateNavigation(item.href)}
                 >
                   {item.name}
-                </Link>
+                </NavLink>
               ))}
             </div>
           </div>
