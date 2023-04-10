75f741cf8b8be6c73763e4c5ce62b8a4b1eb317b
remax21 <deronzier.remi@gmail.com>
2023-04-11 00:16:29 +0200

    :sparkles: (Patients) Fetch patients from API

--- src/pages/patients-page/PatientsPage.jsx
+++ src/pages/patients-page/PatientsPage.jsx
@@ -6,125 +6,55 @@ import {
   PlusCircleIcon,
   TrashIcon,
 } from "@heroicons/react/20/solid";
+import { useEffect, useState } from "react";
 import manAvatar from "../../assets/images/avatar-man.png";
 import womanAvatar from "../../assets/images/avatar-woman.png";
+import Loader from "../../components/loader/Loader";
 
-const people = [
-  {
-    id: 1,
-    name: "Jane Cooper",
-    dateOfBirth: "23/03/1998",
-    sex: "Homme",
-    address: "47, street NYC",
-    telephone: "+1-202-555-0170",
-  },
-  {
-    id: 2,
-    name: "Jane Cooper",
-    dateOfBirth: "23/03/1998",
-    sex: "Homme",
-    address: "47, street NYC",
-    telephone: "+1-202-555-0170",
-  },
-  {
-    id: 3,
-    name: "Jane Cooper",
-    dateOfBirth: "23/03/1998",
-    sex: "Homme",
-    address: "47, street NYC",
-    telephone: "+1-202-555-0170",
-  },
-  {
-    id: 4,
-    name: "Jane Cooper",
-    dateOfBirth: "23/03/1998",
-    sex: "Femme",
-    address: "47, street NYC",
-    telephone: "+1-202-555-0170",
-  },
-  {
-    id: 5,
-    name: "Jane Cooper",
-    dateOfBirth: "23/03/1998",
-    sex: "Femme",
-    address: "47, street NYC",
-    telephone: "+1-202-555-0170",
-  },
-  {
-    id: 6,
-    name: "Jane Cooper",
-    dateOfBirth: "23/03/1998",
-    sex: "Femme",
-    address: "47, street NYC",
-    telephone: "+1-202-555-0170",
-  },
-  {
-    id: 6,
-    name: "Jane Cooper",
-    dateOfBirth: "23/03/1998",
-    sex: "Femme",
-    address: "47, street NYC",
-    telephone: "+1-202-555-0170",
-  },
-  {
-    id: 7,
-    name: "Jane Cooper",
-    dateOfBirth: "23/03/1998",
-    sex: "Femme",
-    address: "47, street NYC",
-    telephone: "+1-202-555-0170",
-  },
-  {
-    id: 8,
-    name: "Jane Cooper",
-    dateOfBirth: "23/03/1998",
-    sex: "Femme",
-    address: "47, street NYC",
-    telephone: "+1-202-555-0170",
-  },
-  {
-    id: 9,
-    name: "Jane Cooper",
-    dateOfBirth: "23/03/1998",
-    sex: "Femme",
-    address: "47, street NYC",
-    telephone: "+1-202-555-0170",
-  },
-  {
-    id: 10,
-    name: "Jane Cooper",
-    dateOfBirth: "23/03/1998",
-    sex: "Femme",
-    address: "47, street NYC",
-    telephone: "+1-202-555-0170",
-  },
-  {
-    id: 10,
-    name: "Jane Cooper",
-    dateOfBirth: "23/03/1998",
-    sex: "Femme",
-    address: "47, street NYC",
-    telephone: "+1-202-555-0170",
-  },
-  {
-    id: 11,
-    name: "Jane Cooper",
-    dateOfBirth: "23/03/1998",
-    sex: "Femme",
-    address: "47, street NYC",
-    telephone: "+1-202-555-0170",
-  },
-  {
-    id: 12,
-    name: "Jane Cooper",
-    dateOfBirth: "23/03/1998",
-    sex: "Femme",
-    address: "47, street NYC",
-    telephone: "+1-202-555-0170",
-  },
-];
+const sexTypes = {
+  M: { label: "Homme", image: manAvatar },
+  F: { label: "Femme", image: womanAvatar },
+};
+
 export default function PatientsPage() {
-  return (
+  const [patients, setPatients] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    let shouldCancel = false;
+    async function fetchPatients() {
+      try {
+        const response = await fetch("http://localhost:8081/patients");
+        if (response.ok) {
+          const data = await response.json();
+          if (shouldCancel) return;
+          setPatients(data.content);
+        } else {
+          alert("Une erreur est survenue");
+        }
+      } catch (error) {
+        alert("Une erreur est survenue");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchPatients();
+    return () => {
+      shouldCancel = true;
+    };
+  }, []);
+
+  const buildFullName = (firstName, lastName) => {
+    return `${firstName} ${lastName.toUpperCase()}`;
+  };
+
+  const buildSex = (sex) => {
+    return sexTypes[sex].label;
+  };
+
+  return isLoading ? (
+    <Loader />
+  ) : (
     <div>
       <button
         type="button"
@@ -137,37 +67,37 @@ export default function PatientsPage() {
         role="list"
         className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4"
       >
-        {people.map((person) => (
+        {patients.map((patient) => (
           <li
-            key={person.id}
+            key={patient.id}
             className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow"
           >
             <div className="flex w-full justify-between space-x-8 p-5">
               <div className="flex-1 truncate">
                 <div className="flex items-center space-x-3 pb-2">
                   <h3 className="truncate text-sm font-medium text-gray-900">
-                    {person.name}
+                    {buildFullName(patient.firstName, patient.lastName)}
                   </h3>
                   <span className="inline-block flex-shrink-0 rounded-full bg-green-100 px-2 py-0.5 text-xs font-medium text-green-800">
-                    {person.sex}
+                    {buildSex(patient.sex)}
                   </span>
                 </div>
                 <div className="flex items-center gap-x-2 text-gray-500">
                   <CakeIcon className="h-4 w-4" aria-hidden="true" />
-                  <p className="mt-1 truncate text-sm">{person.dateOfBirth}</p>
+                  <p className="mt-1 truncate text-sm">{patient.dateOfBirth}</p>
                 </div>
                 <div className="flex items-center gap-x-2 text-gray-500">
                   <MapPinIcon className="h-4 w-4" aria-hidden="true" />
-                  <p className="mt-1 truncate text-sm">{person.address}</p>
+                  <p className="mt-1 truncate text-sm">{patient.address}</p>
                 </div>
                 <div className="flex items-center gap-x-2 text-gray-500">
                   <PhoneIcon className="h-4 w-4" aria-hidden="true" />
-                  <p className="mt-1 truncate text-sm">{person.telephone}</p>
+                  <p className="mt-1 truncate text-sm">{patient.phoneNumber}</p>
                 </div>
               </div>
               <img
                 className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300"
-                src={person.sex === "Homme" ? manAvatar : womanAvatar}
+                src={sexTypes[patient.sex].image}
                 alt=""
               />
             </div>
