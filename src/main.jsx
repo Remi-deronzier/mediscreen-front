9b4a64439564979b95b7be99de033d9c10c93201
remax21 <deronzier.remi@gmail.com>
2023-04-15 19:43:26 +0200

    :recycle: (Patient) Remove api context

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./assets/styles/index.css";
-import { ApiContext, baseUrlServices } from "./context/apiContext";
 import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ApiContext.Provider value={baseUrlServices}>
-      <RouterProvider router={router} />
-    </ApiContext.Provider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
